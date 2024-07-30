d7890fa9f0a2573ad03cbadb26c69313fb55065a
Igor Sikachyna <sikachynairl@gmail.com>
2024-07-30 09:03:54 -0400

    BLOCK-2434 - Update naming, add PR template

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ if (process.argv.includes('--dev')) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: isPages ? '/tool-kit' : '',
+    base: isPages ? '/ultra-tool-kit' : '',
     plugins: defaultPlugins,
     optimizeDeps: {
         esbuildOptions: {
